07296f9c59f92e95b41507add0ce98b628a13097
michaelhaymon <mhaymon2010@gmail.com>
2019-11-26 13:51:32 -0600

    Send to Shopping List now functional

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -12,9 +12,17 @@ export class ShoppingListService {
     return this.ingredients.slice();
   }
 
-
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    // for (let ingredient of ingredients) {
+    //   this.addIngredient(ingredient);
+    // }
+
+    this.ingredients.push(...ingredients);
     this.ingredientsChanged.emit(this.ingredients.slice());
   }
 }
