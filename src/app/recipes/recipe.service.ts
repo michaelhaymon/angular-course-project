43c39087d054f0b2c2c34bdce1458b2d9761be17
michaelhaymon <mhaymon2010@gmail.com>
2019-11-26 09:18:25 -0600

    changed recipe selection to service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,20 +1,22 @@
-import { Injectable } from "@angular/core";
-import { Recipe } from "./recipe.model";
+import { Injectable, EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe(
-      "A Test Recipe",
-      "This is simply a test",
-      "https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg"
+      'A Test Recipe',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
     ),
     new Recipe(
-      "Another Test Recipe",
-      "This is simply another test",
-      "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/3/30/0/0182146_hot-wings_s4x3.jpg.rend.hgtvcom.826.620.suffix/1382543453894.jpeg"
+      'Another Test Recipe',
+      'This is simply another test',
+      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/3/30/0/0182146_hot-wings_s4x3.jpg.rend.hgtvcom.826.620.suffix/1382543453894.jpeg'
     )
   ];
 
@@ -22,5 +24,5 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
-  constructor() {}
+  constructor() { }
 }
